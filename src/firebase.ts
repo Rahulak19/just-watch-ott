48c57398f6db0989806675e45a690f294698da55
Rahulak19 <rahuladhi2011@gmail.com>
2023-07-09 22:35:39 +0530

    Added firebase authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyDDxWOogsCom0548pTmUWD_Vc1iAZvVnDE",
   authDomain: "just-watch-56d9a.firebaseapp.com",
@@ -19,3 +18,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+//const db=app.firestore();
+const auth = getAuth(app);
+
+export {auth};
+//export default db;
--- src/pages/signInScreen/SignInScreen.tsx
+++ src/pages/signInScreen/SignInScreen.tsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./SignInScreen.css"
+import {auth} from "../../firebase"
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 const SignInScreen:React.FC=()=>{
+    const emailRef=useRef<HTMLDivElement | any>()
+    const passwordRef=useRef<HTMLDivElement | any>()
+    const register=(e:React.SyntheticEvent):void=>{
+        e.preventDefault();
+       // const auth = getAuth();
+createUserWithEmailAndPassword(auth, emailRef?.current?.value, passwordRef?.current?.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("user",userCredential)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    // ..
+  });
+        
+    }
+    const signIn=(e:React.SyntheticEvent):void=>{
+        e.preventDefault();
+        signInWithEmailAndPassword(auth,emailRef?.current?.value,passwordRef?.current?.value)
+        .then((signIn)=>{
+            console.log(signIn)
+        })
+        .catch((error)=>{
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+        })
+    }
+
     return(
         <>
         <div className="signIn__screen">
@@ -9,9 +44,13 @@ const SignInScreen:React.FC=()=>{
            
             <form className="signIn__screen__form">
                 <div className="signIn_title">Sign In</div>
-                <input className="signIn__inp" type="email" placeholder="Email" />
-                <input className="signIn__inp" type="password" placeholder="Password" />
-                <button className="signIn__btn" type="submit">Sign In</button>
+                <input className="signIn__inp" ref={emailRef} type="email" placeholder="Email" />
+                <input className="signIn__inp" ref={passwordRef} type="password" placeholder="Password" />
+                <button className="signIn__btn" type="submit" onClick={(e)=>signIn(e)}>Sign In</button>
+                <h5>
+                    <span className="signup__screen__span"> New to Just Watch?</span>
+                  <span className="signup__screen__span__link" onClick={(e)=>register(e)}>Sign Up now.</span> 
+                </h5>
             </form>
             </div>
         </div>
